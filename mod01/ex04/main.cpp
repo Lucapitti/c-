d75541457abe5ac2db93afb35d836c78f6f43dc1
Luca Pittigliani <luca@1ac44cce-d277-4b18-af85-7dbcd4cee61c.fritz.box>
2025-04-06 19:31:43 +0200

    ciao

---
+++ mod01/ex04/main.cpp
@@ -0,0 +1,37 @@
+#include "Sed.hpp"
+
+void	write_to_file(std::ofstream& outputFile, std::ifstream& infile, std::string cmp, std::string sub)
+{
+	std::string content((std::istreambuf_iterator<char>(infile)), std::istreambuf_iterator<char>());
+	size_t pos = 0;
+	while ((pos = content.find(cmp, pos)) != -1)
+	{
+		content.replace(pos, cmp.size(), sub);
+		pos += sub.size();
+	}
+	outputFile << content;
+}
+
+int main(int argc, char **argv){
+	size_t count = 0;
+	std::string file_name = argv[1];
+	size_t len = strlen(argv[2]);
+	if (argc != 4)
+	{
+		std::cout<<"Invalid input, it has to be filename s1 s2"<<std::endl;
+		return (1);
+	}
+	std::ifstream inputFile(argv[1]);
+	if (!inputFile.is_open())
+	{
+		std::cout<<"Error opening file "<<argv[1]<<std::endl;
+		return (1);
+	}
+	std::ofstream outputFile(file_name + ".replace");
+	if (!outputFile.is_open())
+	{
+		std::cout<<"Error opening file "<<argv[1]<<std::endl;
+		return (1);
+	}
+	write_to_file(outputFile, inputFile, argv[2], argv[3]);
+}
