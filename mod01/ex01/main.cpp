9f66bd3252d03b20d65a1f696e40acbc6ad682b1
Luca Pittigliani <lpittigl@e3r6p3.42roma.it>
2025-12-05 13:26:20 +0100

    ciao

--- mod01/ex01/main.cpp
+++ mod01/ex01/main.cpp
@@ -5,11 +5,7 @@ int main()
 	int N = 10;
 	int	i = 0;
 
-	Zombie *Norman = newZombie("Norman");
-	Norman->announce();
-	randomChump("Bizio");
 	Zombie *horde = zombieHorde(10, "Nico");
-	delete (Norman);
 	while (i < N)
 		horde[i++].announce();
 	delete[] (horde);
--- mod01/ex01/randomChump.cpp
+++
@@ -1,7 +0,0 @@
-#include "Zombie.hpp"
-
-void randomChump(std::string name)
-{
-	Zombie tmp = Zombie(name);
-	tmp.announce();
-}
