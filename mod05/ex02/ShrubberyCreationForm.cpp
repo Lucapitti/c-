8e59883fc6301d661a9a775be4ad2fcaa05be936
Luca Pittigliani <luca@MacBook-Air-di-Luca.fritz.box>
2025-12-15 23:36:03 +0100

    icao

--- mod05/ex02/ShrubberyCreationForm.cpp
+++ mod05/ex02/ShrubberyCreationForm.cpp
@@ -1,5 +1,7 @@
 #include "ShrubberyCreationForm.hpp"
 #include <iostream>
+#include <fstream>
+#include <string>
 
 ShrubberyCreationForm::ShrubberyCreationForm() : target("Default")
 {
@@ -32,5 +34,8 @@ void ShrubberyCreationForm::execute(const Bureaucrat& executor)
 		throw AForm::GradeTooLowException();
 	std::ofstream file;
 	file.open((target + "_shrubbery").c_str());
-	if ("")
+	if (!file.is_open())
+		throw std::runtime_error("Could not open file!");
+	file << "    ccee88oo\n  C8O8O8Q8PoOb o8oo\n dOB69QO8PdUOpugoO9bD\nCgggbU8OU qOp qOdoUOdcb\n    6OuU  /p u gcoUodpP\n      \\\\\\//  /douUP\n        \\\\\\\\////\n         |||/\\ \n         ||\\/\n         |||||\n   .....//||||\\....." << std::endl;
+	file.close();
 }
